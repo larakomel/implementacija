e373848d6ef7488f54faad8ad9e38519ef775d07
larakomel <riki1lara@gmail.com>
2021-01-16 10:17:31 +0100

    profile page structure

--- profile.php
+++ profile.php
@@ -2,10 +2,36 @@
  include_once 'header.php';
  ?>
 <br>
-   
+<div class="stran">
+  <div class = "profil">
+    <div class="profilna">
+      <img src="logo.png" alt="logo" style="width:70px;">
+    </div>
+    <h3> Ime Priimek </h3>
+    <div class="izbira">
+      <a class="nav-link" style="color: black; font-size: 20px;" href="omeni.php">O meni</a>  
+    </div>
+</div>
+<br>
+
+
+</div>
+
+<div class="narocila">
+  <h5>Moja naročila</h5>
+</div>
+<div class="stran">
+<div class="narocilo">
+<h6> Naročilo: 65436756734</h6>
+   <h6> Naročilo še ni bilo sprejeto </h6>
+   <ul>
+  <li>Coffee</li>
+  <li>Tea</li>
+  <li>Milk</li>
+</ul>
+  </div>
+</div>
    
 
-    
-    </div>
   </body>
 </html>
